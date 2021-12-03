8144c6fbc607eb0d18fd47516151ec7a2f416474
Surapa <surapa152@gmail.com>
2021-12-03 17:00:53 +0700

    lab8

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -2,7 +2,17 @@
 using namespace std;
 
 char before(char x){
-	//Write your function definition here
+	if(x < 'A' ||x > 'Z'){
+		x = '0';
+	}
+	else{
+		if(x == 'A'){
+			return 'Z';
+		}
+		x = x - 1;
+
+		}	
+	return x;	
 }
 
 int main(){
